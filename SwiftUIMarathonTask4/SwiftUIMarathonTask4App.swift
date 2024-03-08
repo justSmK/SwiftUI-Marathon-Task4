771cc572408ef2f91390ec1d589d39396084fc53
justSmK <28645140+justSmK@users.noreply.github.com>
2024-03-08 10:35:34 +0300

    Init

---
+++ SwiftUIMarathonTask4/SwiftUIMarathonTask4App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMarathonTask4App.swift
+//  SwiftUIMarathonTask4
+//
+//  Created by Sergei Semko on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMarathonTask4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
